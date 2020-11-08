c4175e50e85083a2e7d5cd0e2a955681b41daf53
mikhawa <michaeljpitz@gmail.com>
2020-11-08 19:33:50 +0100

    ORDER BY rubriques_order

--- model/rubriquesModel.php
+++ model/rubriquesModel.php
@@ -1,7 +1,7 @@
 <?php
 // Select all rubriques
 function selectAllRubriques($c){
-    $sql = "SELECT * FROM rubriques;";
+    $sql = "SELECT * FROM rubriques ORDER BY rubriques_order ASC;";
     $req = mysqli_query($c,$sql) or die(mysqli_error($c));
     return (mysqli_num_rows($req))? mysqli_fetch_all($req,MYSQLI_ASSOC):[];
 }
@@ -18,11 +18,12 @@ function createMenu(int $parent, int $level, array $rubriques){
             $out .="    <li><a href='?id={$node['idrubriques']}'>".$node['rubriques_name']."</a></li>";
             $prevLevel=$level;
             $out .= createMenu($node['idrubriques'],($level+1),$rubriques);
-        }
-    }
 
-    if(($prevLevel==$level) && ($prevLevel !=0)) $out .= "</ul>\n\n";
-    elseif($prevLevel==$level) $out .= "\n</ul>\n";
+        }
+
+    }
+    if(($prevLevel==$level) && ($prevLevel !=0)) $out .= "</ul>\n";
+    elseif($prevLevel==$level) $out .= "</ul>\n";
     else $out .="\n";
 
     return $out;
